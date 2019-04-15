3baedabf076038a543659d8bc7706511773e1e08
Hao Ye <lhopitalified@gmail.com>
2019-04-15 13:50:37 -0400

    update example files

---
+++ run.R
@@ -0,0 +1,4 @@
+library(portalcasting)
+
+my_config <- readRDS("my_config.RDS")
+portalcast(my_config)
--- setup.R
+++ setup.R
@@ -1,9 +1,21 @@
-# load the `portalcasting` package
 library(portalcasting)
 
-# define the folder where we are going to store all the components for making 
-#  portal predictions
-base_dir <- here::here("portal_predictions")
+#### configure how we're going to run portalcasting ----
 
-# use `portalcasting::setup_dir` to create the `portal_predictions`` folders
-setup_dir(all_options(base = base_dir))
+# 1. define the folder where we are going to store everything
+base <- here::here("portal_predictions")
+
+# 2. restrict the models to just the AutoArima and the new model we are developing
+models <- c("AutoArima", "EDM_simplex")
+
+# n. generate the full options list and save the configuration
+my_config <- all_options(base = base, 
+                         models = models)
+saveRDS(my_config, file = "my_config.RDS")
+
+#### use `portalcasting::setup_dir` to create the folders ----
+my_config <- readRDS("my_config.RDS")
+if (!file.exists(my_config$options_dir$tree$base))
+{
+    setup_dir(my_config)
+}
